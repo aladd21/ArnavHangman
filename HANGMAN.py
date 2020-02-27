24d8959d856ca148c8bb4c3140bd20d0cf609614
aladd21 <aladd21@students.shrewsbury.k12.ma.us>
2020-02-27 13:19:35 -0500

    Update HANGMAN.py
    changed according to arnav's, simple changes

--- HANGMAN.py
+++ HANGMAN.py
@@ -2,10 +2,33 @@ from __future__ import print_function
 updatedSpaces=['-','-','-','-','-']
 word='steve'
 lives=5
+def initialize():
+    word = 'steve'
+    print['-','-','-','-','-']
+
+def getLetter():
+    print('Guess a letter.')
+    letter = raw_input()
+    global letter
+    
+def won():
+    if letter in word:
+        print('Yay, you won!!') 
+    if letter not in word:
+        print(getLetter)      
+        global letter
+          
+                         
+def main():
+    initialize()
+    getLetter()                
+
+                     
 def check(letter):
     global lives
     global updatedSpaces
     global word
+    global letter
     if letter in word:
         for i in range(0,len(word)-1):
             Hamza=word.find(letter, i, len(word))
